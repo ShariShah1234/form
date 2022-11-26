6d2c4dd74e3fa514508458ef5afcddca4bebc14e
ShariShah1234 <muhammadshaheryar74@gmail.com>
2022-11-26 15:41:23 +0500

    commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var getval= document.getElementById("name1")
+var errorIn=document.getElementById("name2")
+var arr=[]
+function getValue(){
+console.log(getval.value)
+var a=getval.value
+if(a==""){
+errorIn.innerHTML="User Name"}
+else{
+arr.push(a)  
+console.log(arr)
+}}
+var getval= document.getElementById("name3")
+var errorIn=document.getElementById("name4")
+var arr=[]
+function getName(){
+console.log(getval.value)
+var a=getval.value
+if(a==""){
+errorIn.innerHTML="Father Name"}
+else{arr.push(a)  
+console.log(arr)
+}}
+var getval= document.getElementById("name5")
+var errorIn=document.getElementById("name6")
+var arr=[]
+function getContact(){
+console.log(getval.value)
+var a=getval.value
+if(a==""){
+errorIn.innerHTML="Contact"}
+else{arr.push(a)  
+console.log(arr)
+}}
+var getval= document.getElementById("name7")
+var errorIn=document.getElementById("name8")
+var arr=[]
+function getCNIC(){
+console.log(getval.value)
+var a=getval.value
+if(a==""){
+errorIn.innerHTML="CNIC"}
+else{arr.push(a)  
+console.log(arr)
+}}
+var getval= document.getElementById("name9")
+var errorIn=document.getElementById("name10")
+var arr=[]
+function getEmail(){
+console.log(getval.value)
+var a=getval.value
+if(a==""){
+errorIn.innerHTML="Email"}
+else{arr.push(a)  
+console.log(arr)
+}}
+var getval= document.getElementById("name11")
+var errorIn=document.getElementById("name12")
+var arr=[]
+function getPassword(){
+console.log(getval.value)
+var a=getval.value
+if(a==""){
+errorIn.innerHTML="Password"}
+else{arr.push(a)  
+console.log(arr)
+}}
